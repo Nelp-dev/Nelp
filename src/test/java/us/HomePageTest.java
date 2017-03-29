1e4b50641882a1e9fc2c999a5d492d8a98ad3d68
Kwon Young Jae <kyoje11@gmail.com>
2017-03-29 14:13:24 +0900

    Update Homepage Function Test & Add SSoid from user
    Update Homepage Function Test & Add SSoid from user

--- src/test/java/us/HomePageTest.java
+++ src/test/java/us/HomePageTest.java
@@ -1,29 +1,50 @@
 package us;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.hamcrest.CoreMatchers.*;
+
+import static org.hamcrest.CoreMatchers.is;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HomePageTest {
     private WebDriver driver;
+    private String baseURL = "http://localhost:8080/";
     @Before
-    public void setUpSelenium(){
-        System.setProperty("webdriver.chrome.driver", "src/test/driver/chromedriver-mac");
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "src/test/driver/chromedriver");
         driver = new ChromeDriver();
+    }
+    @After
+    public void tearDown(){
+        driver.quit();
     }
 
     @Test
     public void test_home_title(){
-        driver.get("http://localhost:8080/");
-        Assert.assertThat(driver.getTitle(),is("홈"));
-        driver.quit();
+        driver.get(baseURL);
+        Assert.assertThat(driver.getTitle(),is("Nelp"));
+    }
+
+    @Test
+    public void test_home_login(){
+        driver.get(baseURL);
+        driver.findElement(By.id("login")).click();
+        Assert.assertThat(driver.getCurrentUrl(),is(baseURL+"login"));
+    }
+
+    @Test
+    public void test_home_signup(){
+        driver.get(baseURL);
+        driver.findElement(By.id("signup")).click();
+        Assert.assertThat(driver.getCurrentUrl(),is(baseURL+"signup"));
     }
 }
