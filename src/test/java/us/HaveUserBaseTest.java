28ea1f8f489b206a1d40605db45e25f6d7dcf09f
cjh5414 <cjh5414@gmail.com>
2017-04-05 12:06:25 +0900

    Add PaymentTest

--- src/test/java/us/HaveUserBaseTest.java
+++ src/test/java/us/HaveUserBaseTest.java
@@ -8,13 +8,25 @@ public class HaveUserBaseTest extends BaseTest {
     @Autowired
     protected UserRepository userRepository;
     protected User test_meeting_maker;
+    protected User test_meeting_participation;
 
     @Override
     public void setUp() {
-        test_meeting_maker = new User("test@email.com", "test meeting maker", "test account number", "test password");
+        test_meeting_maker = new User(
+                "test@email.com",
+                "test meeting maker",
+                "test account number",
+                "test password");
         if(!userRepository.exists(test_meeting_maker.getSsoId()))
             userRepository.save(test_meeting_maker);
+        test_meeting_participation = new User(
+                "test2@email.com",
+                "test meeting participation",
+                "test account number2",
+                "test password");
+        if(!userRepository.exists(test_meeting_participation.getSsoId()))
+            userRepository.save(test_meeting_participation);
+
         super.setUp();
     }
-
 }
---
+++ src/test/java/us/PaymentTest.java
@@ -0,0 +1,16 @@
+package us;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaymentTest extends HaveUserBaseTest {
+    @Test
+    public void test_show_payment_info_in_meeting_detail_page() {
+
+
+    }
+}
