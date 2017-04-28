b07604cd53a62fac5a65d1b3da4645f89e6ec32d
sim4858 <sim4858@naver.com>
2017-04-28 18:29:53 +0900

    Remove unused import file

--- src/main/java/us/controller/MeetingController.java
+++ src/main/java/us/controller/MeetingController.java
@@ -3,9 +3,6 @@ package us.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +11,6 @@ import us.model.*;
 import us.repository.*;
 
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 import java.util.*;
 
 @Controller
