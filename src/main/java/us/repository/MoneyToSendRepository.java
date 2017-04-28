e5ca65a6d5d9638f3fa9a098e54e619de7a24940
Kwon Young Jae <kyoje11@gmail.com>
2017-04-28 16:10:49 +0900

    Function Removing Money_To_Send, Payment

--- src/main/java/us/repository/MoneyToSendRepository.java
+++ src/main/java/us/repository/MoneyToSendRepository.java
@@ -1,13 +1,13 @@
 package us.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import us.model.Meeting;
 import us.model.MoneyToSend;
+import us.model.Payment;
 import us.model.User;
 
 import java.util.List;
 
 public interface MoneyToSendRepository extends CrudRepository<MoneyToSend, Integer> {
-    List<MoneyToSend> findByMeeting(Meeting meeting);
+    List<MoneyToSend> findByPayment(Payment payment);
     MoneyToSend findBySender(User sender);
 }
