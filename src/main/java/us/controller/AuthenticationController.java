7bdc7d3e0bf809ca397559b38d5692f154864d74
sim4858 <sim4858@naver.com>
2017-05-09 20:29:43 +0900

    make temporary password

--- src/main/java/us/controller/AuthenticationController.java
+++ src/main/java/us/controller/AuthenticationController.java
@@ -58,4 +58,36 @@ public class AuthenticationController {
         userRepository.save(user);
         return "redirect:/";
     }
+
+    public String getTemporaryPassword() {
+        int index = 0;
+        int PASSWORD_LENGTH = 7;
+        char[] charSet = new char[]{
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
+                'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        };
+
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < PASSWORD_LENGTH; i++) {
+            index = (int) (charSet.length * Math.random());
+            sb.append(charSet[index]);
+        }
+        return sb.toString();
+    }
+
+    public boolean changePassword(String ssoId, String newPassword) {
+        for(User getUser:userRepository.findAll()) {
+            if(getUser.getSsoId().equals(ssoId)) {
+                getUser.setPassword(newPassword);
+                userRepository.save(getUser);
+                return true;
+            }
+        }
+        return false;
+    }
 }
