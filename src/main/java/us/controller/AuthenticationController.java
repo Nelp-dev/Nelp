ecc89a7ac0a8cfede808f70ba9d6526867d72ca4
sim4858 <sim4858@naver.com>
2017-05-14 14:32:23 +0900

    Insert change_password page

--- src/main/java/us/controller/AuthenticationController.java
+++ src/main/java/us/controller/AuthenticationController.java
@@ -58,4 +58,33 @@ public class AuthenticationController {
         userRepository.save(user);
         return "redirect:/";
     }
+
+    @GetMapping("/changepassword")
+    public String changePassword(Model model) {
+        model.addAttribute("tempUser", new User());
+        model.addAttribute("isNotEqualPassword", false);
+        model.addAttribute("isNotEqualTwoNewPassword", false);
+
+        return "change_password";
+    }
+
+    @PostMapping("/changepassword")
+    public String changePassword(User user, Model model, HttpSession session) {
+        User updateUser = userRepository.findOne(((User)session.getAttribute("user")).getSsoId());
+        if(user.getSsoId().equals(user.getName())) {
+            if(user.getPassword().equals(updateUser.getPassword())) {
+                updateUser.setPassword(user.getSsoId());
+                userRepository.save(updateUser);
+                return "redirect:/mypage";
+            }
+            else {
+                model.addAttribute("isNotEqualPassword", true);
+                model.addAttribute("tempUser", new User());
+                return "change_password";
+            }
+        }
+        model.addAttribute("isNotEqualTwoNewPassword", true);
+        model.addAttribute("tempUser", new User());
+        return "change_password";
+    }
 }
